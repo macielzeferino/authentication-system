b4957de7021493db7143f9d568df4a95124af4ee
Maciel Zeferino <maciel.zeferino0@gmail.com>
2025-03-12 09:06:22 -0300

    controler product

---
+++ src/main/java/com/macielzeferino/authenticationsystem/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.macielzeferino.authenticationsystem.controller;
+
+import com.macielzeferino.authenticationsystem.entity.Product;
+import com.macielzeferino.authenticationsystem.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+}
